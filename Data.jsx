c399ac1c1c6442113b5c3e8f0d264b73674156f5
snehuu24 <124239168+snehuu24@users.noreply.github.com>
2023-10-25 12:38:17 +0530

    Add files via upload

---
+++ Data.jsx
@@ -0,0 +1,19 @@
+const Data =[
+    {name : "Alia Bhat", age : 28, 
+    img : "https://upload.wikimedia.org/wikipedia/commons/8/88/Alia_Bhatt.jpg"},
+   
+    {name : "Virat Kohli", age : 30, 
+    img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuXjjCcO7RA7BSi5duE_SKh0N_CG2BxVRiXptCymUV&s"},
+    
+    {name : "Rashmika", age : 29,
+     img : "http://t2.gstatic.com/images?q=tbn:ANd9GcTTiG7m7Or22bEwwy7HC1UFtxL8tln9PqJdzoOamk-tkeTv_ls6BlDrHRA-5w0t_4P6ySryeA"},
+    
+     {name : "Vijay D", age : 40, 
+    img :"https://wallpapers.com/images/featured/vijay-devarakonda-4k-9u8z4s1v32x96tkw.jpg"},
+   
+    {name : "Shah Rukh Khan", age : 50,
+     img : "https://w0.peakpx.com/wallpaper/518/289/HD-wallpaper-shahrukh-khan-srk-smile-srk-smile-thumbnail.jpg"}
+
+]
+
+export default Data
